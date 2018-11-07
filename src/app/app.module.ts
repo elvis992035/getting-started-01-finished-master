a397b204cd7f7f18b12616951fb938340289748a
elvis992035 <elvis992035@gmail.com>
2018-11-06 22:49:51 -0500

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+//import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
@@ -8,6 +13,8 @@ import { SearchComponent } from './search/search.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProductComponent } from './product/product.component';
 import { StarsComponent } from './stars/stars.component';
+
+
 
 @NgModule({
   declarations: [
@@ -20,7 +27,12 @@ import { StarsComponent } from './stars/stars.component';
     StarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //AppBootstrapModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
